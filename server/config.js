460624173b7c3877f9bcb82cf63e77937b527487
Amit Dubey <amitdubey8888@gmail.com>
2026-01-12 20:09:51 +0530

    Security: Refactor secrets to use environment variables

--- server/config.js
+++ server/config.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-  database: 'mongodb+srv://wallet:wallet@wallet.wlg0mke.mongodb.net/?retryWrites=true&w=majority&appName=wallet',
-  server_secret: '$2y$12$4A2phQdjCl9fFvcuTHpn3.l7mZTBNagz4HrApfnY1tXYcyHgalifK',
-  token_expirey: '1d',
+  database: process.env.DATABASE,
+  server_secret: process.env.SERVER_SECRET,
+  token_expirey: process.env.TOKEN_EXPIREY,
 };
+
